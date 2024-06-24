65dcbc1535df7f2050e10b00163a968e8a1f1974
matheusrodacki <44122620+matheusrodacki@users.noreply.github.com>
2024-06-24 12:07:21 -0300

    feat: :sparkles: adiciona o router

---
+++ config/routes.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+  'GET|/' => \Alura\MVC\Controller\VideoListController::class,
+  'GET|/novo-video' => \Alura\MVC\Controller\VideoFormController::class,
+  'POST|/novo-video' => \Alura\MVC\Controller\NewVideoController::class,
+  'GET|/editar-video' => \Alura\MVC\Controller\VideoFormController::class,
+  'POST|/editar-video' => \Alura\MVC\Controller\EditVideoController::class,
+  'GET|/remover-video' => \Alura\MVC\Controller\DeleteVideoController::class,
+];
