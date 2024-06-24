c2a2d1dea783323042e26bdfe1cebfd63398b4a3
matheusrodacki <44122620+matheusrodacki@users.noreply.github.com>
2024-06-24 11:44:09 -0300

    feat: :sparkles: add views files

---
+++ Views/video-list.php
@@ -0,0 +1,20 @@
+<?php
+require_once __DIR__ . '/head.php';
+require_once __DIR__ . '/cabecalho.php'; ?>
+
+<ul class="videos__container" alt="videos alura">
+  <?php foreach ($videoList as $video) : ?>
+    <li class="videos__item">
+      <iframe width="100%" height="72%" src=<?php echo $video->url ?>" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      <div class="descricao-video">
+        <img src="./img/logo.png" alt="logo canal alura">
+        <h3><?php echo $video->titulo ?></h3>
+        <div class="acoes-video">
+          <a href="/editar-video?id=<?= $video->id ?>">Editar</a>
+          <a href="/deleta-video?id=<?= $video->id ?>">Excluir</a>
+        </div>
+      </div>
+    </li>
+  <?php endforeach; ?>
+</ul>
+<?php require_once __DIR__ . '/footer.php';
--- src/Controller/VideoListController.php
+++ src/Controller/VideoListController.php
@@ -16,25 +16,6 @@ class VideoListController
   public function processaRequisicao(): void
   {
     $videoList = $this->videoRepository->all();
-
-    require_once __DIR__ . '/../../head.php';
-    require_once __DIR__ . '/../../cabecalho.php'; ?>
-
-    <ul class="videos__container" alt="videos alura">
-      <?php foreach ($videoList as $video) : ?>
-        <li class="videos__item">
-          <iframe width="100%" height="72%" src=<?php echo $video->url ?>" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-          <div class="descricao-video">
-            <img src="./img/logo.png" alt="logo canal alura">
-            <h3><?php echo $video->titulo ?></h3>
-            <div class="acoes-video">
-              <a href="/editar-video?id=<?= $video->id ?>">Editar</a>
-              <a href="/deleta-video?id=<?= $video->id ?>">Excluir</a>
-            </div>
-          </div>
-        </li>
-      <?php endforeach; ?>
-    </ul>
-<?php require_once __DIR__ . '/../../footer.php';
+    require __DIR__ . '/../../Views/video-list.php';
   }
 }
