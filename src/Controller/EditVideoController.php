d623700eea7fde10b4b9a040fb45901edb9061c3
Matheus Rodacki <44122620+matheusrodacki@users.noreply.github.com>
2024-07-01 15:19:35 +0000

    replica alterações de upload no edit

--- src/Controller/EditVideoController.php
+++ src/Controller/EditVideoController.php
@@ -36,11 +36,18 @@ class EditVideoController implements Controller
     $video = new Video($url, $titulo);
 
     if ($_FILES['image']['error'] === UPLOAD_ERR_OK) {
+      $safeFileName = uniqid('upload_') . '_' . pathinfo($_FILES['image']['name'], PATHINFO_BASENAME);
+      $finfo = new \finfo(FILEINFO_MIME_TYPE);
+      $mimeType = $finfo->file($_FILES['image']['tmp_name']);
+      if (str_starts_with($mimeType, 'image/')) {
+        header('Location: /?sucesso=0');
+        return;
+      }
       move_uploaded_file(
         $_FILES['image']['tmp_name'],
-        __DIR__ . '/../../public/uploads/' . $_FILES['image']['name']
+        __DIR__ . '/../../public/uploads/' . $safeFileName
       );
-      $video->setImagePath($_FILES['image']['name']);
+      $video->setImagePath($safeFileName);
     }
 
     $video->setId($id);
